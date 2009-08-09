c754c01fa5c2650cbd90327101842eb7402b6d22
weavejester <jreeves@weavejester.com>
2009-08-09 19:35:44 +0100

    Added basic package finding

--- src/capra/package.clj
+++ src/capra/package.clj
@@ -1,12 +1,22 @@
 (ns capra.package
   "An extensible package manager for Clojure."
+  (:refer-clojure :exclude [find])
   (:import java.net.URL)
   (:import java.io.InputStreamReader)
   (:import java.io.PushbackReader))
 
+;; Environment
+
+(def *home-dir*
+  (or (System/getenv "CAPRA_HOME")
+      (System/getenv "HOME")))
+
+;; Sources
+
 (def sources (atom []))
 
-(def source-type-regex #"(^[a-z0-9.-]+)\+")
+(def #^{:private true}
+  source-type-regex #"(^[a-z0-9.-]+)\+")
 
 (defn- get-source-type
   "Gets the type of the source. The type is encoded in the scheme of the URL.
@@ -15,14 +25,14 @@
   (second (re-find source-type-regex source)))
 
 (defn- strip-source-type
-  "Strip the source type from the URL. For example, 'capra+http://example.com'
+  "Strips the source type from the URL. For example, 'capra+http://example.com'
   becomes 'http://example.com'."
   [url]
   (-> (re-matcher source-type-regex url)
     (.replaceAll "")))
 
 (defmulti read-source
-  "Read package metadata from a source."
+  "Reads package metadata from a source."
   get-source-type)
 
 (defmethod read-source "capra"
@@ -31,3 +41,23 @@
         stream (.openStream (URL. url))
         reader (PushbackReader. (InputStreamReader. stream))]
     (read reader)))
+
+(defn read-all-sources
+  "Reads all sources in capra.package/sources"
+  []
+  (apply merge (map read-source @sources)))
+
+;; Packages
+
+(defn- matches-version?
+  "Returns true if the version matches the package."
+  [version package]
+  (= (package :version) version))
+
+(defn find
+  "Finds a package in the repository"
+  [name version]
+  (let [repository (read-all-sources)
+        packages   (repository name)]
+    (first (filter (partial matches-version? version)
+                   packages))))
