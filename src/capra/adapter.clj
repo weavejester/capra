4bc1ec2d240ac1f7eb54a21e02ef08f935d4e8a2
weavejester <jreeves@weavejester.com>
2009-10-08 21:28:30 +0100

    Removed a whole bunch of stuff

--- src/capra/adapter.clj
+++
@@ -1,8 +0,0 @@
-(ns capra.adapter
-  "Multimethods used for reading a package repository. Can be overridden to
-  support different repository types.")
-
-(defmulti query-package
-  "Given a group, name and version of a package in a repository, return a map
-  of the package metadata."
-  (fn [type source group name version] type))
