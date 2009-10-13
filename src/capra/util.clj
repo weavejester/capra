bb0589872d3b44c32b57b0ea99dbaf1a744b6769
weavejester <jreeves@weavejester.com>
2009-10-13 21:29:15 +0100

    Added base64-encode function

--- src/capra/util.clj
+++ src/capra/util.clj
@@ -1,5 +1,6 @@
 (ns capra.util
   "Utility functions for Capra."
+  (:refer-clojure :exclude [->>])
   (:import java.io.File)
   (:import java.io.InputStream)
   (:import java.io.OutputStream)
@@ -43,3 +44,47 @@
         (when (pos? len)
           (.write out buffer 0 len)
           (recur (.read in buffer)))))))
+
+(defmacro #^{:private true} ->>
+  "Backported from Clojure HEAD."
+  ([x form] (if (seq? form)
+              `(~(first form) ~@(next form)  ~x)
+              (list form x)))
+  ([x form & more] `(->> (->> ~x ~form) ~@more)))
+
+(def base64-chars
+  (vec "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"))
+
+(defn- bytes->int
+  "Bitshift three bytes into an integer."
+  [[x y z]]
+  (+ (bit-shift-left x 16)
+     (bit-shift-left y 8)
+     z))
+
+(defn- encode-bits
+  "Encode part of an integer into a Base64 character."
+  [i n]
+  (base64-chars (bit-and (bit-shift-right i n) 0x3f)))
+
+(defn- encode-padded-bytes
+  "Encode a collection of padded bytes into Base64."
+  [bytes]
+  (mapcat
+    (fn [chunk]
+      (let [i (bytes->int chunk)]
+         (map #(encode-bits i %) [18 12 6 0])))
+    (partition 3 bytes)))
+
+(defn base64-encode
+  "Encode a list of bytes into a Base64 string."
+  [bytes]
+  (let [pad-size (mod (- 3 (mod (count bytes) 3)) 3)
+        pad-with (fn [x coll]
+                   (concat coll (repeat pad-size x)))]
+    (->> bytes
+      (pad-with (byte 0))
+      (encode-padded-bytes)
+      (drop-last pad-size)
+      (pad-with \=)
+      (apply str))))
