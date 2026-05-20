9aee7631e98cad1f81fdbb97db6398fdb125dd14
James Reeves <jreeves@weavejester.com>
2026-05-20 12:13:17 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject dev.weavejester/capra "0.1.0-SNAPSHOT"
+  :description "A fast Ring adapter"
+  :url "https://github.com/weavejester/capra"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.12.5"]])
---
+++ test/capra/server_test.clj
@@ -0,0 +1,5 @@
+(ns capra.server-test
+  (:require [clojure.test :refer [deftest is]]))
+
+(deftest a-test
+  (is (= 0 1)))
