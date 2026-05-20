be6994200fd9fa183a88bce71f1ac3169e5f6b01
James Reeves <jreeves@weavejester.com>
2026-05-20 21:23:10 +0100

    Add beginnings of HTTP parsing

--- src/capra/server.clj
+++ src/capra/server.clj
@@ -1,1 +1,24 @@
-(ns capra.server)
+(ns capra.server
+  (:require [clojure.string :as str]
+            [teensyp.buffer :as buf]
+            [teensyp.server :as tcp])
+  (:import [java.nio ByteBuffer]
+           [java.nio.charset StandardCharsets]))
+
+(defn- http-handler
+  ([_socket]
+   (transient {}))
+  ([state socket ^ByteBuffer buffer]
+   (when-some [line (buf/read-line buffer StandardCharsets/US_ASCII)]
+     (let [[method uri protocol] (str/split line #" ")]
+       (assoc! state
+               :request-method method
+               :uri uri
+               :protocol protocol))
+     (tcp/close socket)
+     state))
+  ([state _exception]
+   (prn (persistent! state))))
+
+(defn start-server [options]
+  (tcp/start-server (assoc options :handler #'http-handler)))
