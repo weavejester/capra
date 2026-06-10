cef4a948acbd4d6e257918cacdf81469334afc66
James Reeves <jreeves@weavejester.com>
2026-06-10 13:44:51 +0100

    Add response Content-Length test

--- test/capra/server_test.clj
+++ test/capra/server_test.clj
@@ -3,21 +3,39 @@
             [capra.server :as capra]
             [clj-http.client :as http]))
 
-(defn- hello-world-handler [_request]
-  {:status  200
-   :headers {"Content-Type" "text/plain"}
-   :body    "Hello World"})
-
 (deftest request-response-test
-  (with-open [_ (capra/start-server hello-world-handler {:port 4321})]
+  (with-open [_ (capra/start-server
+                 (fn handler [_request]
+                   {:status  200
+                    :headers {"Content-Type" "text/plain"}
+                    :body    "Hello World"})
+                 {:port 4321})]
     (let [response (http/get "http://localhost:4321")]
       (is (= {:status  200
               :headers {"Content-Type"      "text/plain"
                         "Server"            "Capra"
                         "Transfer-Encoding" "chunked"}
               :body    "Hello World"}
-             (-> response 
+             (-> response
                  (select-keys [:status :headers :body])
                  (update :headers dissoc "Date"))))
       (is (re-matches #"\w{3}, \d+ \w{3} \d{4} \d\d:\d\d:\d\d GMT"
                       (get-in response [:headers "Date"]))))))
+
+(deftest response-content-length-test
+  (with-open [_ (capra/start-server
+                 (fn handler [_request]
+                   {:status  200
+                    :headers {"Content-Type"   "text/plain"
+                              "Content-Length" "11"}
+                    :body    "Hello World"})
+                 {:port 4322})]
+    (let [response (http/get "http://localhost:4322")]
+      (is (= {:status  200
+              :headers {"Content-Type"   "text/plain"
+                        "Content-Length" "11"
+                        "Server"         "Capra"}
+              :body    "Hello World"}
+             (-> response
+                 (select-keys [:status :headers :body])
+                 (update :headers dissoc "Date")))))))
