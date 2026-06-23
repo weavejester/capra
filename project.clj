0aaebeeb1a07004c312fc1f0ccf9c963467391d9
James Reeves <jreeves@weavejester.com>
2026-06-23 13:28:48 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.12.5"]
-                 [dev.weavejester/teensyp "0.7.0"]
-                 [org.ring-clojure/ring-core-protocols "1.15.4"]]
+                 [dev.weavejester/teensyp "0.7.3"]
+                 [org.ring-clojure/ring-core-protocols "1.15.5"]]
   :plugins [[cider/cider-nrepl "0.59.0"]]
   :global-vars {*warn-on-reflection* true}
   :profiles {:dev {:dependencies [[clj-http "3.13.1"]
