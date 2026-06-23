f6a0ab19cfd0874372580d5570ca4464fca09c63
James Reeves <jreeves@weavejester.com>
2026-06-23 17:41:48 +0100

    Add automated benchmark using wrk

--- bench/capra/benchmark.clj
+++ bench/capra/benchmark.clj
@@ -1,4 +1,19 @@
-(ns capra.benchmark)
+(ns capra.benchmark
+  (:require [capra.server :as capra]
+            [clojure.java.shell :as shell]))
+
+(defn simple-handler [_request]
+  {:status  200
+   :headers {"Content-Type" "text/plain; charset=UTF-8"}
+   :body    "Hello World"})
+
+(defn wrk [{:keys [port duration connections threads]
+            :or   {connections 1, threads 1}}]
+  (shell/sh "wrk" "-d" duration "-c" (str connections) "-t" (str threads)
+            (str "http://localhost:" port)))
 
 (defn -main []
-  (println "Benchmarking goes here."))
+  (with-open [_ (capra/start-server simple-handler {:port 5800})]
+    (println "Benchmarking Capra...")
+    (println (:out (wrk {:port 5800 :duration "5s"})))
+    (shutdown-agents)))
