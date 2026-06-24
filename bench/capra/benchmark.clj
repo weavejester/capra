8d892ed27a12c6bfa0127a700771229357f189d7
James Reeves <jreeves@weavejester.com>
2026-06-24 17:06:14 +0100

    Update benchmark duration and wrk config

--- bench/capra/benchmark.clj
+++ bench/capra/benchmark.clj
@@ -10,20 +10,20 @@
    :body    "Hello World"})
 
 (defn wrk [{:keys [port duration connections threads]
-            :or   {connections 1, threads 1}}]
+            :or   {connections 10, threads 2}}]
   (shell/sh "wrk" "-d" duration "-c" (str connections) "-t" (str threads)
             (str "http://localhost:" port)))
 
 (defn -main []
   (with-open [_ (capra/start-server simple-handler {:port 5800})]
     (println "Benchmarking Capra...")
-    (println (:out (wrk {:port 5800 :duration "5s"}))))
+    (println (:out (wrk {:port 5800 :duration "1m"}))))
   (let [close (httpkit/run-server simple-handler {:port 5801})]
     (try (println "Benchmarking HTTP-Kit...")
-         (println (:out (wrk {:port 5801 :duration "5s"})))
+         (println (:out (wrk {:port 5801 :duration "1m"})))
          (finally (close))))
   (let [server (jetty/run-jetty simple-handler {:port 5802, :join? false})]
     (try (println "Benchmarking Ring Jetty...")
-         (println (:out (wrk {:port 5802 :duration "5s"})))
+         (println (:out (wrk {:port 5802 :duration "1m"})))
          (finally (.stop ^org.eclipse.jetty.server.Server server))))
   (shutdown-agents))
