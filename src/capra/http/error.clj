938aa3d3f3262819e42778987ec3e5da197968a7
James Reeves <jreeves@weavejester.com>
2026-07-02 02:18:13 +0100

    Refactor capra.http.error namespace

--- src/capra/http/error.clj
+++ src/capra/http/error.clj
@@ -1,50 +1,31 @@
 (ns capra.http.error)
 
+(defn- plaintext-response [status body]
+  {:status  status
+   :headers {"Connection" "close"
+             "Content-Type" "text/plain; charset=UTF-8"}
+   :body    body})
+
 (def error-handlers
   {:unsupported-transfer-encoding
    (fn [{{:strs [transfer-encoding]} :headers}]
-     {:status  501
-      :headers {"Connection" "close"
-                "Content-Type" "text/plain; charset=UTF-8"}
-      :body    (str "Unsupported request transfer encoding: \""
-                    transfer-encoding "\".\n"
-                    "Only \"chunked\" transfer encoding supported.")})
+     (plaintext-response
+      501 (str "Unsupported request transfer encoding: \"" transfer-encoding
+               "\".\nOnly \"chunked\" transfer encoding supported.")))
    :uri-too-long
-   (constantly
-    {:status  414
-     :headers {"Connection" "close"
-               "Content-Type" "text/plain; charset=UTF-8"}
-     :body    "URI too long."})
+   (constantly (plaintext-response 414 "URI too long."))
    :request-header-field-too-large
-   (constantly
-    {:status  431
-     :headers {"Connection" "close"
-               "Content-Type" "text/plain; charset=UTF-8"}
-     :body    "Request header field too large."})
+   (constantly (plaintext-response 431 "Request header field too large."))
    :missing-host-header
-   (constantly
-    {:status  400
-     :headers {"Connection" "close"
-               "Content-Type" "text/plain; charset=UTF-8"}
-     :body    "Missing \"Host\" header in request."})
+   (constantly (plaintext-response 400 "Missing \"Host\" header in request."))
    :http-version-not-supported
    (fn [{:keys [bad-protocol]}]
-     {:status  505
-      :headers {"Connection" "close"
-                "Content-Type" "text/plain; charset=UTF-8"}
-      :body    (str "Unsupported HTTP version: \"" bad-protocol "\".\n"
-                    "Only \"HTTP/1.0\" and \"HTTP/1.1\" supported.")})
+     (plaintext-response
+      505 (str "Unsupported HTTP version: \"" bad-protocol
+               "\".\nOnly \"HTTP/1.0\" and \"HTTP/1.1\" supported.")))
    :invalid-request-start-line
-   (constantly
-    {:status  400
-     :headers {"Connection" "close"
-               "Content-Type" "text/plain; charset=UTF-8"}
-     :body    "Invalid HTTP request start line."})
+   (constantly (plaintext-response 400 "Invalid HTTP request start line."))
    :invalid-request-header
    (fn [{:keys [bad-header]}]
-     {:status  400
-      :headers {"Connection" "close"
-                "Content-Type" "text/plain; charset=UTF-8"}
-      :body    (str "Invalid HTTP request header line: \"" bad-header "\".")})})
-
-
+     (plaintext-response
+      400 (str "Invalid HTTP request header line: \"" bad-header "\".")))})
