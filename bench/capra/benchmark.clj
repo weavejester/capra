883a5a946fd864ea77f4f92c3e3a2bb92be56da4
James Reeves <jreeves@weavejester.com>
2026-07-05 13:07:02 +0100

    Improve benchmarking script
    Add warmups, hide error logs and ensure thread pools are consistent in
    benchmarking script.

--- bench/capra/benchmark.clj
+++ bench/capra/benchmark.clj
@@ -2,7 +2,9 @@
   (:require [capra.server :as capra]
             [clojure.java.shell :as shell]
             [org.httpkit.server :as httpkit]
-            [ring.adapter.jetty :as jetty]))
+            [ring.adapter.jetty :as jetty])
+  (:import [org.eclipse.jetty.server Server]
+           [org.eclipse.jetty.util.thread VirtualThreadPool]))
 
 (defn simple-handler [_request]
   {:status  200
@@ -15,15 +17,26 @@
             (str "http://localhost:" port)))
 
 (defn -main []
-  (with-open [_ (capra/run-server simple-handler {:port 5800})]
+  (with-open [_ (capra/run-server simple-handler
+                                  :port 5800
+                                  :error-logger (fn [_]))]
+    (println "Warming up Capra...")
+    (wrk {:port 5800 :duration "5s"})
     (println "Benchmarking Capra...")
     (println (:out (wrk {:port 5800 :duration "1m"}))))
   (let [close (httpkit/run-server simple-handler {:port 5801})]
-    (try (println "Benchmarking HTTP-Kit...")
+    (try (println "Warming up http-kit...")
+         (wrk {:port 5801 :duration "5s"})
+         (println "Benchmarking HTTP-Kit...")
          (println (:out (wrk {:port 5801 :duration "1m"})))
          (finally (close))))
-  (let [server (jetty/run-jetty simple-handler {:port 5802, :join? false})]
-    (try (println "Benchmarking Ring Jetty...")
+  (let [server (jetty/run-jetty simple-handler
+                                {:port 5802
+                                 :join? false
+                                 :thread-pool (VirtualThreadPool.)})]
+    (try (println "Warming up Ring Jetty...")
+         (wrk {:port 5802 :duration "5s"})
+         (println "Benchmarking Ring Jetty...")
          (println (:out (wrk {:port 5802 :duration "1m"})))
-         (finally (.stop ^org.eclipse.jetty.server.Server server))))
+         (finally (.stop ^Server server))))
   (shutdown-agents))
