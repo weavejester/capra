ac297a20d0cb93f66da13683e54aeff774c15b67
James Reeves <jreeves@weavejester.com>
2026-07-06 12:53:41 +0100

    Add benchmarking for hot workloads

--- bench/capra/benchmark.clj
+++ bench/capra/benchmark.clj
@@ -6,10 +6,21 @@
   (:import [org.eclipse.jetty.server Server]
            [org.eclipse.jetty.util.thread VirtualThreadPool]))
 
+(def ^:const hot-work-timeout 40)
+
 (defn simple-handler [_request]
   {:status  200
    :headers {"Content-Type" "text/plain; charset=UTF-8"}
    :body    "Hello World"})
+
+(defn hot-handler [_request]
+  (let [t0 (System/currentTimeMillis)]
+    (loop [n 0.0]
+      (if (< (- (System/currentTimeMillis) t0) hot-work-timeout)
+        (recur (+ n (Math/random)))
+        {:status  200
+         :headers {"Content-Type" "text/plain; charset=UTF-8"}
+         :body    (str "Result: " n)}))))
 
 (defn wrk [{:keys [port duration connections threads]
             :or   {connections 128, threads 2}}]
@@ -19,20 +30,20 @@
 (defn -main []
   (println "Process ID: " (.pid (java.lang.ProcessHandle/current)))
   (Thread/sleep 2000)
-  (with-open [_ (capra/run-server simple-handler
+  (with-open [_ (capra/run-server hot-handler
                                   :port 5800
                                   :error-logger (fn [_]))]
     (println "Warming up Capra...")
     (wrk {:port 5800 :duration "5s"})
     (println "Benchmarking Capra...")
     (println (:out (wrk {:port 5800 :duration "1m"}))))
-  (let [close (httpkit/run-server simple-handler {:port 5801})]
+  (let [close (httpkit/run-server hot-handler {:port 5801})]
     (try (println "Warming up http-kit...")
          (wrk {:port 5801 :duration "5s"})
          (println "Benchmarking HTTP-Kit...")
          (println (:out (wrk {:port 5801 :duration "1m"})))
          (finally (close))))
-  (let [server (jetty/run-jetty simple-handler
+  (let [server (jetty/run-jetty hot-handler
                                 {:port 5802
                                  :join? false
                                  :thread-pool (VirtualThreadPool.)})]
